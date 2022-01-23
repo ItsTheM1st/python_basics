94438b7afa48c8097a406dbb1ab785b849a2de3e
M1ST <93210061+ItsTheM1st@users.noreply.github.com>
2022-01-23 21:08:00 +0700

    First commit

---
+++ Introduction/intro.py
@@ -0,0 +1,8 @@
+print("Welcome to Python!")
+print("Python is awesome!!")
+
+#Welcome to Python!
+#Python is awesome!!
+
+
+#day finished : 20st of Jan, 2022
---
+++ Syntax/Comments/comments.py
@@ -0,0 +1,11 @@
+#This is a totally normal comment
+#yay! another comment!
+
+print("Weeeeeeeeeeeeeeeeeeee") #a lot of 'e's !
+
+'''
+
+dang. now u understand comments!
+congrats!!!
+
+'''
